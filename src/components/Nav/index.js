99d38bb0ad8adef4640db8b119ff1213dae23b24
Clement-Bresson <clement0bresson@gmail.com>
2018-09-10 18:51:34 +0100

    Add logo

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,16 +5,22 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 import LoginButton from '../LoginButton'
+import LogoImage from './logo.png'
 
-const styles = {
+const styles = (theme) => ({
+  logo: {
+    marginRight: theme.spacing.unit * 2,
+    width: '50px'
+  },
   flex: {
     flexGrow: 1
   }
-}
+})
 
 const Nav = ({ classes }) => (
   <AppBar position="static" color="primary">
     <Toolbar>
+      <img className={classes.logo} alt="Diadem Network logo" src={LogoImage} />
       <Typography variant="title" color="inherit" className={classes.flex}>
         Diadem Network
       </Typography>

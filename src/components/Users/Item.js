d60dad5c8db4bfde8b3a9bff613b3d01dee9e578
Clement-Bresson <clement0bresson@gmail.com>
2018-09-14 13:28:27 +0100

    Add facebook profile link

--- src/components/Users/Item.js
+++ src/components/Users/Item.js
@@ -5,14 +5,24 @@ import ListItemText from '@material-ui/core/ListItemText'
 import Avatar from '@material-ui/core/Avatar'
 import Typography from '@material-ui/core/Typography'
 import PermIdentityIcon from '@material-ui/icons/PermIdentityOutlined'
+import { withStyles } from '@material-ui/core/styles'
 
-const UserItem = ({ user }) => {
+const styles = (theme) => ({
+  link: {
+    color: theme.palette.primary.light,
+    textDecoration: 'none'
+  }
+})
+
+const UserItem = ({ classes, user }) => {
   const { userAccount, userName } = user
   return (
     <ListItem>
       <Avatar><PermIdentityIcon /></Avatar>
       <ListItemText primary={[
-        <Typography color="textSecondary" key='time'>{userAccount}</Typography>,
+        <Typography color="textSecondary" key='time'>
+          <a className={classes.link} href={`https://www.facebook.com/profile.php?id=${userAccount}`} target="_blank">see facebook profile</a>
+        </Typography>,
         <Typography key='text'>
           {userName}
         </Typography>
@@ -22,7 +32,8 @@ const UserItem = ({ user }) => {
 }
 
 UserItem.propTypes = {
+  classes: T.object,
   user: T.object
 }
 
-export default UserItem
+export default withStyles(styles)(UserItem)

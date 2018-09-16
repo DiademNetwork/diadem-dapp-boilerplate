8cf3686c6d784884f1968e7bdca3272c8d926f77
Clement-Bresson <clement0bresson@gmail.com>
2018-09-16 15:24:17 +0100

    Fix timeline

--- src/components/Timeline/Item.js
+++ src/components/Timeline/Item.js
@@ -27,45 +27,43 @@ const TimelineItem = ({ classes, transaction }) => {
   const txComponent = <a className={classes.link} href={`${process.env.QTUM_INSIGHT_URL}/tx/${target}`} target="_blank">view qtum transaction on blockchain explorer</a>
   const formattedTime = moment.utc(time).from(moment.utc(new Date()))
   const nameDisplay = name || actor
-  const achievementLink = object && <a className={classes.link} href={object} target="_blank">achievement</a>
+  const achievementLink = <a className={classes.link} href={object} target="_blank">achievement</a>
   switch (verb) {
     case 'create':
       icon = <StarIcon />
-      mainText = `${nameDisplay} created ${achievementLink}`
+      mainText = (<Typography key='text'>{nameDisplay} created {achievementLink}</Typography>)
       break
     case 'update':
       icon = <StarIcon />
-      mainText = `${nameDisplay} updated ${achievementLink}`
+      mainText = (<Typography key='text'>{nameDisplay} updated {achievementLink}</Typography>)
       break
     case 'support':
       icon = <MoneyIcon />
-      mainText = `${nameDisplay} supported ${achievementLink}`
+      mainText = (<Typography key='text'>{nameDisplay} supported {achievementLink}</Typography>)
       break
     case 'deposit':
       icon = <VpnKeyOutlinedIcon />
-      mainText = `${nameDisplay} deposited for ${achievementLink} with witness ${witnessName}`
+      mainText = (<Typography key='text'>{nameDisplay} deposited for {achievementLink} with witness {witnessName}</Typography>)
       break
     case 'confirm':
       icon = <DoneIcon />
-      mainText = `${nameDisplay} confirmed ${achievementLink}`
+      mainText = (<Typography key='text'>{nameDisplay} confirmed {achievementLink}</Typography>)
       break
     case 'register':
       icon = <PermIdentityIcon />
-      mainText = `${nameDisplay} registered with address ${object}`
+      mainText = (<Typography key='text'>{nameDisplay} registered with address {object}</Typography>)
       break
     case 'withdraw':
     default:
       icon = <RemoveIcon />
-      mainText = `${nameDisplay} withdraw`
+      mainText = (<Typography key='text'>{nameDisplay} withdraw</Typography>)
   }
   return (
     <ListItem>
       <Avatar>{icon}</Avatar>
       <ListItemText primary={[
         <Typography color="textSecondary" key='time'>{formattedTime}</Typography>,
-        <Typography key='text'>
-          {mainText}
-        </Typography>,
+        mainText,
         txComponent && <Typography key='tx'>{txComponent}</Typography>
       ]} />
     </ListItem>

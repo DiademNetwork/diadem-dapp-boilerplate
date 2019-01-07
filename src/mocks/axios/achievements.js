d363218e063eabea4b01068f9ee533215f5e6c70
Clement-Bresson <clement0bresson@gmail.com>
2019-01-07 19:48:31 +0000

    fix mocks

--- src/mocks/axios/achievements.js
+++ src/mocks/axios/achievements.js
@@ -3,10 +3,9 @@ import { createMockResponse } from './utils'
 export default (axiosMock) => {
   axiosMock.onPost('/confirm').reply(createMockResponse(200))
   axiosMock.onPost(/^\/.+\/(create)$/).reply(createMockResponse(200))
-  axiosMock.onPost('/create').reply(createMockResponse(200))
-  axiosMock.onPost('/deposit').reply(createMockResponse(200))
-  axiosMock.onPost('/support').reply(createMockResponse(200))
-  axiosMock.onPost('/encode-deposit').reply(createMockResponse(200, { address: 'QetMQCLKHswMsU3NZg9MtWR3R9r9479CAT', encodedData: '68656c6c6f7720776f726c64' }))
-  axiosMock.onPost('/encode-support').reply(createMockResponse(200, { address: 'QetMQCLKHswMsU3NZg9MtWR3R9r9479CAT', encodedData: '68656c6c6f7720776f726c64' }))
+  axiosMock.onPost(/^\/.+\/(deposit)$/).reply(createMockResponse(200))
+  axiosMock.onPost(/^\/.+\/(support)$/).reply(createMockResponse(200))
+  axiosMock.onPost(/^\/.+\/(encode-deposit)$/).reply(createMockResponse(200, { address: 'QetMQCLKHswMsU3NZg9MtWR3R9r9479CAT', encodedData: '68656c6c6f7720776f726c64' }))
+  axiosMock.onPost(/^\/.+\/(encode-support)$/).reply(createMockResponse(200, { address: 'QetMQCLKHswMsU3NZg9MtWR3R9r9479CAT', encodedData: '68656c6c6f7720776f726c64' }))
   return axiosMock
 }

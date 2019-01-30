a4062aa0ff1ae7b9b72d2100bddeb6211d6d71be
Clement-Bresson <clement0bresson@gmail.com>
2019-01-30 15:35:28 +0000

    Optimize only for prod

--- webpackConfigs/webpack.production.js
+++ webpackConfigs/webpack.production.js
@@ -1,10 +1,28 @@
 const webpackConfigMerger = require('./webpackConfigMerger')
 const stagingWebpackConfig = require('./webpack.staging')
+const TerserPlugin = require('terser-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
 
 module.exports = webpackConfigMerger(stagingWebpackConfig, {
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin()
+    ],
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor_app',
+          chunks: 'all',
+          minChunks: 2
+        }
+      }
+    }
+  },
   // In CI, env variables are passed directly
   // and so are not needed to be added
   plugins: process.env.BACKEND_URL ? [] : [
--- webpackConfigs/webpack.staging.js
+++ webpackConfigs/webpack.staging.js
@@ -1,5 +1,4 @@
 const webpackConfigMerger = require('./webpackConfigMerger')
-const TerserPlugin = require('terser-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
 const commonWebpackConfig = require('./webpack.common')
@@ -7,23 +6,6 @@ const commonWebpackConfig = require('./webpack.common')
 module.exports = webpackConfigMerger(commonWebpackConfig, {
   devtool: 'inline-source-map',
   mode: 'production',
-  optimization: {
-    minimizer: [
-      new TerserPlugin()
-    ],
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor_app',
-          chunks: 'all',
-          minChunks: 2
-        }
-      }
-    }
-  },
   // In CI, env variables are passed directly
   // and so are not needed to be added
   plugins: process.env.BACKEND_URL ? [] : [

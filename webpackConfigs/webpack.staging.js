b9b8a09dedb930bc95375faa3ceacbc1b165ff68
Clement-Bresson <clement0bresson@gmail.com>
2019-01-30 14:49:09 +0000

    WIP

--- webpackConfigs/webpack.staging.js
+++ webpackConfigs/webpack.staging.js
@@ -1,4 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const webpackConfigMerger = require('./webpackConfigMerger')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
@@ -8,15 +7,6 @@ module.exports = webpackConfigMerger(commonWebpackConfig, {
   devtool: 'inline-source-map',
   mode: 'production',
   plugins: [
-    new UglifyJsPlugin({
-      sourceMap: true,
-      uglifyOptions: {
-        mangle: {
-          safari10: true,
-          keep_fnames: true
-        }
-      }
-    }),
     // In CI, env variables are passed directly
     // and so are not needed to be added
     process.env.BACKEND_URL ? new Dotenv({ path: path.join(__dirname, '../envs/.staging.env') }) : null

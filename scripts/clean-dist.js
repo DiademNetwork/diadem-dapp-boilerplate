ea402c8e1e76549078b85fdca0252f5460855d5f
Clement-Bresson <clement0bresson@gmail.com>
2019-01-30 11:07:44 +0000

    Simplify deleting dist folder process

--- scripts/clean-dist.js
+++ scripts/clean-dist.js
@@ -1,10 +1,28 @@
 #! /usr/bin/env node
 // Do not remove line above. Use to call this script without having to specify 'node'
 
-const rimraf = require('rimraf')
-const path = require('path')
 const chalk = require('chalk')
+const fs = require('fs')
 
-rimraf(path.join(__dirname, '../dist'), () => {
-  console.log(chalk.green('dist/ folder emptied successfully'))
-})
+function deleteFolderRecursive (path) {
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    fs.readdirSync(path).forEach(function (file) {
+      const curPath = `${path}/${file}`
+
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath)
+      } else { // delete file
+        fs.unlinkSync(curPath)
+      }
+    })
+
+    console.log(chalk.green(`Deleting directory "${path}"...`))
+    fs.rmdirSync(path)
+  }
+};
+
+console.log(chalk.green('Cleaning working tree...'))
+
+deleteFolderRecursive('./dist')
+
+console.log(chalk.green('Successfully cleaned working tree!'))

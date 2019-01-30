7c797f906ea3ded208e85be5f90700af27e6f095
Clement-Bresson <clement0bresson@gmail.com>
2019-01-30 14:59:45 +0000

    Fix config

--- webpackConfigs/webpack.production.js
+++ webpackConfigs/webpack.production.js
@@ -1,29 +1,10 @@
 const webpackConfigMerger = require('./webpackConfigMerger')
+const stagingWebpackConfig = require('./webpack.staging')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
-const testnetWebpackConfig = require('./webpack.testnet')
-const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = webpackConfigMerger(testnetWebpackConfig, {
+module.exports = webpackConfigMerger(stagingWebpackConfig, {
   devtool: 'source-map',
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new TerserPlugin()
-    ],
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor_app',
-          chunks: 'all',
-          minChunks: 2
-        }
-      }
-    }
-  },
   plugins: [
     // In CI, env variables are passed directly
     // and so are not needed to be added
--- webpackConfigs/webpack.staging.js
+++ webpackConfigs/webpack.staging.js
@@ -1,4 +1,6 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const webpackConfigMerger = require('./webpackConfigMerger')
+const TerserPlugin = require('terser-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
 const commonWebpackConfig = require('./webpack.common')
@@ -6,7 +8,33 @@ const commonWebpackConfig = require('./webpack.common')
 module.exports = webpackConfigMerger(commonWebpackConfig, {
   devtool: 'inline-source-map',
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin()
+    ],
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor_app',
+          chunks: 'all',
+          minChunks: 2
+        }
+      }
+    }
+  },
   plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        mangle: {
+          safari10: true,
+          keep_fnames: true
+        }
+      }
+    }),
     // In CI, env variables are passed directly
     // and so are not needed to be added
     process.env.BACKEND_URL ? new Dotenv({ path: path.join(__dirname, '../envs/.staging.env') }) : null

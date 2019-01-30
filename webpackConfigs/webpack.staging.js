3c9823891f74e3a029892784e69928f24f3cef4d
Clement-Bresson <clement0bresson@gmail.com>
2019-01-30 15:11:46 +0000

    WIP

--- webpackConfigs/webpack.staging.js
+++ webpackConfigs/webpack.staging.js
@@ -26,15 +26,7 @@ module.exports = webpackConfigMerger(commonWebpackConfig, {
     }
   },
   plugins: [
-    new UglifyJsPlugin({
-      sourceMap: true,
-      uglifyOptions: {
-        mangle: {
-          safari10: true,
-          keep_fnames: true
-        }
-      }
-    }),
+    new UglifyJsPlugin(),
     // In CI, env variables are passed directly
     // and so are not needed to be added
     process.env.BACKEND_URL ? new Dotenv({ path: path.join(__dirname, '../envs/.staging.env') }) : null

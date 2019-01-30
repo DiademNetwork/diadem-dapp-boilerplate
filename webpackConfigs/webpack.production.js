12b480fac028286f0aa27067b987563c51e39173
Bresson <clement0bresson@gmail.com>
2019-01-30 16:28:46 +0000

    [Fix] CircleCI Pipeline (#43)
    -  change diadem loom URL
    - fix and improve circleCI config

--- webpackConfigs/webpack.production.js
+++ webpackConfigs/webpack.production.js
@@ -1,32 +1,23 @@
+const webpack = require('webpack')
 const webpackConfigMerger = require('./webpackConfigMerger')
+const stagingWebpackConfig = require('./webpack.staging')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
-const testnetWebpackConfig = require('./webpack.testnet')
-const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = webpackConfigMerger(testnetWebpackConfig, {
+module.exports = webpackConfigMerger(stagingWebpackConfig, {
   devtool: 'source-map',
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new TerserPlugin()
-    ],
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor_app',
-          chunks: 'all',
-          minChunks: 2
-        }
-      }
-    }
-  },
-  plugins: [
-    // In CI, env variables are passed directly
-    // and so are not needed to be added
-    process.env.BACKEND_URL ? new Dotenv({ path: path.join(__dirname, '../envs/.production.env') }) : null
+  // In CI, env variables will be available already (so process.env.BACKEND_URL will already exists)
+  // and so are not needed to be added
+  plugins: process.env.BACKEND_URL ? [
+    new webpack.EnvironmentPlugin([
+      'BACKEND_URL',
+      'GETSTREAM_APP_KEY',
+      'GETSTREAM_APP_ID',
+      'GETSTREAM_ACHIEVEMENT_COMMON_TOKEN',
+      'SUPPORT_CONTACT_EMAIL',
+      'NODE_ENV'
+    ])
+  ] : [
+    new Dotenv({ path: path.join(__dirname, '../envs/.production.env') })
   ]
 })
--- webpackConfigs/webpack.staging.js
+++ webpackConfigs/webpack.staging.js
@@ -1,24 +1,42 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
 const webpackConfigMerger = require('./webpackConfigMerger')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 const commonWebpackConfig = require('./webpack.common')
 
 module.exports = webpackConfigMerger(commonWebpackConfig, {
   devtool: 'inline-source-map',
   mode: 'production',
-  plugins: [
-    new UglifyJsPlugin({
-      sourceMap: true,
-      uglifyOptions: {
-        mangle: {
-          safari10: true,
-          keep_fnames: true
+  optimization: {
+    minimizer: [
+      new TerserPlugin()
+    ],
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor_app',
+          chunks: 'all',
+          minChunks: 2
         }
       }
-    }),
-    // In CI, env variables are passed directly
-    // and so are not needed to be added
-    process.env.BACKEND_URL ? new Dotenv({ path: path.join(__dirname, '../envs/.staging.env') }) : null
+    }
+  },
+  // In CI, env variables are passed directly
+  // and so are not needed to be added
+  plugins: process.env.BACKEND_URL ? [
+    new webpack.EnvironmentPlugin([
+      'BACKEND_URL',
+      'GETSTREAM_APP_KEY',
+      'GETSTREAM_APP_ID',
+      'GETSTREAM_ACHIEVEMENT_COMMON_TOKEN',
+      'SUPPORT_CONTACT_EMAIL',
+      'NODE_ENV'
+    ])
+  ] : [
+    new Dotenv({ path: path.join(__dirname, '../envs/.staging.env') })
   ]
 })

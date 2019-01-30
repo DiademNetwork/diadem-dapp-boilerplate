d8eb224017eb748de7b822e32035a5d432e8fb14
Clement-Bresson <clement0bresson@gmail.com>
2019-01-30 15:44:23 +0000

    Fix empty env variables?

--- webpackConfigs/webpack.production.js
+++ webpackConfigs/webpack.production.js
@@ -1,8 +1,11 @@
+const webpack = require('webpack')
 const webpackConfigMerger = require('./webpackConfigMerger')
 const stagingWebpackConfig = require('./webpack.staging')
 const TerserPlugin = require('terser-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
+
+console.log(process.env.BACKEND_URL)
 
 module.exports = webpackConfigMerger(stagingWebpackConfig, {
   devtool: 'source-map',
@@ -23,9 +26,18 @@ module.exports = webpackConfigMerger(stagingWebpackConfig, {
       }
     }
   },
-  // In CI, env variables are passed directly
+  // In CI, env variables will be available already (so process.env.BACKEND_URL will already exists)
   // and so are not needed to be added
-  plugins: process.env.BACKEND_URL ? [] : [
+  plugins: process.env.BACKEND_URL ? [
+    new webpack.DefinePlugin({
+      'process.env.BACKEND_URL': process.env.BACKEND_URL,
+      'process.env.GETSTREAM_APP_KEY': process.env.GETSTREAM_APP_KEY,
+      'process.env.GETSTREAM_APP_ID': process.env.GETSTREAM_APP_ID,
+      'process.env.GETSTREAM_ACHIEVEMENT_COMMON_TOKEN': process.env.GETSTREAM_ACHIEVEMENT_COMMON_TOKEN,
+      'process.env.SUPPORT_CONTACT_EMAIL': process.env.SUPPORT_CONTACT_EMAIL,
+      'process.env.NODE_ENV': process.env.NODE_ENV
+    })
+  ] : [
     new Dotenv({ path: path.join(__dirname, '../envs/.production.env') })
   ]
 })
--- webpackConfigs/webpack.staging.js
+++ webpackConfigs/webpack.staging.js
@@ -1,14 +1,27 @@
+const webpack = require('webpack')
 const webpackConfigMerger = require('./webpackConfigMerger')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
 const commonWebpackConfig = require('./webpack.common')
+
+console.log(process.env.BACKEND_URL)
+console.log(!!process.env.BACKEND_URL)
 
 module.exports = webpackConfigMerger(commonWebpackConfig, {
   devtool: 'inline-source-map',
   mode: 'production',
   // In CI, env variables are passed directly
   // and so are not needed to be added
-  plugins: process.env.BACKEND_URL ? [] : [
+  plugins: process.env.BACKEND_URL ? [
+    new webpack.DefinePlugin({
+      'process.env.BACKEND_URL': process.env.BACKEND_URL,
+      'process.env.GETSTREAM_APP_KEY': process.env.GETSTREAM_APP_KEY,
+      'process.env.GETSTREAM_APP_ID': process.env.GETSTREAM_APP_ID,
+      'process.env.GETSTREAM_ACHIEVEMENT_COMMON_TOKEN': process.env.GETSTREAM_ACHIEVEMENT_COMMON_TOKEN,
+      'process.env.SUPPORT_CONTACT_EMAIL': process.env.SUPPORT_CONTACT_EMAIL,
+      'process.env.NODE_ENV': process.env.NODE_ENV
+    })
+  ] : [
     new Dotenv({ path: path.join(__dirname, '../envs/.staging.env') })
   ]
 })
